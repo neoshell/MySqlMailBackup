86c84c64a7c23237fd101dda502eb06b30c00203
NeoShell <contact.neoshell@gmail.com>
2017-06-13 22:56:01 -0700

    Create script

---
+++ mysql_mail_backup.sh
@@ -0,0 +1,24 @@
+# Set your config here =========================================================
+
+mysql_user=your_user
+mysql_pass=your_pass
+mysql_database=your_database
+temp_dir=/your/temp/dir/
+mail_address=your_mail@gmail.com
+
+#===============================================================================
+
+date_string=$(date +%Y%m%d)
+
+sql_file=$mysql_database'_'$date_string'.sql'
+mysqldump -u$mysql_user -p$mysql_pass --databases $mysql_database > $temp_dir$sql_file
+
+tar_gz_file=$sql_file'.tar.gz'
+tar zcfP $temp_dir$tar_gz_file $temp_dir$sql_file
+
+mail_subject='Database Backup: '$mysql_database
+mail_content=$tar_gz_file
+echo $mail_content | mutt -s "$mail_subject" -a $temp_dir$tar_gz_file -- $mail_address
+
+rm $temp_dir$tar_gz_file
+rm $temp_dir$sql_file
